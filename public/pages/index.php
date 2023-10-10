4895c5a9df76b65a4aa5fd860a4bc6c24f87f4ba
Guilherme de Almeida Soares <guilherme.soares@estiva.com.br>
2023-10-10 16:57:04 -0300

    Primeiro Commit

---
+++ public/pages/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Lista de Tarefas</title>
+</head>
+<body>
+    <header class="container mt-5">
+        <h1>Lista de Tarefas</h1>
+    </header>
+</body>
+</html>
